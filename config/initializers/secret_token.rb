b5580fdb9cdeaa3a9c784e4b3c130cf5fd7dedda
srctek <info@srctek.com>
2013-01-05 11:14:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crm::Application.config.secret_token = 'c033812ed2a8d658722ded7b2fc9d6b06f53905334795bd3c8ec633890bc9cea1221cf502e75db9e7e9a09371f328fd2c7f44e088a7a1de04fd0c635c2bc3012'
